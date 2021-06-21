17266b49e2511edcaa21a028dc1b8a1b221830cd
yeijSong <83809741+yeijSong@users.noreply.github.com>
2021-06-21 21:57:44 +0900

    Add files via upload

---
+++ 21.05.10/Python06_ifEx02_송예지.py
@@ -0,0 +1,9 @@
+# vId="Orange", pwd="1234" 
+# 만약에 vId가 "Red"이고 pwd가"1234"이면 "반갑습니다.회원님" 
+# 아니면"회원 확인 하세요."
+vId="Red"
+Pwd="1234"
+if vId=="Red" and Pwd=="1234":
+    print("반갑습니다.회원님")
+else:
+	print("회원 확인 하세요.")
